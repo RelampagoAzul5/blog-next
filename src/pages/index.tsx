eb6d5535fa8f72c2b215ce064c2516600ff1d904
Giovanni Henrique <giovannihpf@gmail.com>
2025-04-18 15:09:55 -0300

    Buscando os dados dos posts da api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,29 @@
-export default function Home() {
-  return <h1>oi</h1>;
+import { PostData } from '@/domain/posts/posts';
+import { GetStaticProps } from 'next';
+
+const getPosts = async (): Promise<PostData[]> => {
+  const posts = await fetch(`${process.env.API_URL}/posts`);
+  const jsonPosts = await posts.json();
+  return jsonPosts.data;
+};
+
+export type HomeProps = {
+  posts: PostData[];
+};
+
+export default function Home({ posts }: HomeProps) {
+  return (
+    <div>
+      {posts.map((post) => (
+        <h2 key={post.slug}> {post.title}</h2>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const posts = await getPosts();
+  return {
+    props: { posts },
+  };
+};
